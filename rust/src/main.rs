4ecf5122a69eaf650ca48febb4bdff5cd564d100
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-27 13:31:43 -0500

    Add method to get min index

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
+    let seq_list: Vec<Vec<u128>> = Vec::new();
+    let ind = get_min_index(&seq_list);
+
     println!("{}", calculate_sequence_length(989345275647));
+    println!("{}", ind);
 }
 
 fn calculate_sequence_length(mut n: u128) -> u128 {
@@ -10,4 +14,23 @@ fn calculate_sequence_length(mut n: u128) -> u128 {
     }
 
     return counter
+}
+
+fn get_min_index(seq_list: &Vec<Vec<u128>>) -> usize {
+    let mut min_ind: usize = 0;
+
+    for (i, pair) in seq_list.iter().enumerate() {
+        let min_pair = &seq_list[min_ind];
+
+        if pair[1] < min_pair[1] {
+            min_ind = i;
+        }
+        else if pair[1] == min_pair[1] {
+            if pair[0] > min_pair[0] {
+                min_ind = i;
+            }
+        }
+    }
+
+    return min_ind
 }
