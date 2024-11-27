915f19e208d49e944155208a63bbd4c75e4485d1
Rajwol Chapagain <Rajwol.Chapagain@live.mercer.edu>
2024-11-27 14:37:46 -0500

    Add remaining functionality

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let seq_list: Vec<Vec<u128>> = Vec::new();
-    let ind = get_min_index(&seq_list);
+    let mut seq_list: Vec<Vec<u128>> = Vec::new();
 
-    println!("{}", calculate_sequence_length(989345275647));
-    println!("{}", ind);
+    let args: Vec<String> = std::env::args().collect();
+    let start: u128 = args[1].parse().expect("Not a valid number!"); 
+    let end: u128 = args[2].parse().expect("Not a valid number!"); 
+
+    for i in start..=end {
+        let seq_len = calculate_sequence_length(i);
+        compare_and_add(i, seq_len, &mut seq_list);
+    }
+
+    print_by_seq_len(&seq_list);
+    print_by_n(&seq_list);
 }
 
 fn calculate_sequence_length(mut n: u128) -> u128 {
@@ -33,4 +41,44 @@ fn get_min_index(seq_list: &Vec<Vec<u128>>) -> usize {
     }
 
     return min_ind
+}
+
+fn compare_and_add(n: u128, seq_len:u128, seq_list: &mut Vec<Vec<u128>>) {
+    for pair in &mut *seq_list {
+        if seq_len == pair[1] {
+            return
+        }
+    }
+
+    if seq_list.len() < 10 {
+        seq_list.push(vec![n, seq_len]);
+        return;
+    }
+
+    let min_ind = get_min_index(&seq_list);
+    let min_pair = &seq_list[min_ind];
+
+    if seq_len > min_pair[1] {
+        seq_list[min_ind] = vec![n, seq_len];
+    }
+}
+
+fn print_by_seq_len(seq_list: &Vec<Vec<u128>>) {
+    println!("Sorted based on sequence length");
+    let mut sorted_vec = seq_list.clone();
+    sorted_vec.sort_by(|a, b| b[1].cmp(&a[1]));
+
+    for item in sorted_vec {
+        println!("{:>20} {:>20}", item[0], item[1]);
+    }
+}
+
+fn print_by_n(seq_list: &Vec<Vec<u128>>) {
+    println!("Sorted based on integer size");
+    let mut sorted_vec = seq_list.clone();
+    sorted_vec.sort_by(|a, b| b[0].cmp(&a[0]));
+
+    for item in sorted_vec {
+        println!("{:>20} {:>20}", item[0], item[1]);
+    }
 }
